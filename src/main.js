aacecb0f319c1938f467cbb41233ebb6a2ee8270
swaatipandey <swati.pandey@contentstack.com>
2020-12-10 21:06:44 +0530

    deleted unwanted parts and changed function

--- src/main.js
+++ src/main.js
@@ -1,17 +1,9 @@
 import { data } from "./data.js";
 import { renderEmployeeCard } from "./component/employeeDiv.js";
-// window.onload = ()=>{
-// const rootDiv = document.getElementById("root");
-// data.then((employees) =>{
-//     employees.forEach((employee)=>{
-//         rootDiv.appendChild(renderEmployeeCard(employees));
-//     });
-// })
-// };
+
 const rootDiv = document.getElementById("root");
 data.then((employees) => {
   employees.forEach((employee) => {
     rootDiv.appendChild(renderEmployeeCard(employee));
-    // console.log(employee);
   });
 });
